d84dbb534e98f2f6534c7d7d615f8b125642d339
abhithakre <68857990+abhithakre@users.noreply.github.com>
2020-08-26 17:18:43 +0530

    Add files via upload

--- Cover/Program.cs
+++ Cover/Program.cs
@@ -13,5 +13,36 @@ namespace Cover
         {
             return a + b;
         }
+        public class Employee
+        {
+            public int empId { get; set; }
+            public string empName { get; set; }
+
+        }
+
+        public bool BookTime(Employee employee, DateTime date, decimal duration)
+        {
+            if (employee.empId <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Employee id cannot be 0");
+            }
+
+            if (date.Date < DateTime.Today || date.Date > DateTime.Today)
+            {
+                throw new ArgumentOutOfRangeException("You can book for today only.");
+            }
+
+            if (duration > 9)
+            {
+                throw new ArgumentOutOfRangeException("You can book for 9hrs only");
+            }
+
+
+            return true;
+
+            //return _bookingProcessor.Create(employee.empId,date,duration);
+        }
+
+      
     }
 }
