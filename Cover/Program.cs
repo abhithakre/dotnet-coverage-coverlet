3fd7126273d67fdb0524a0490bd4cecbe9fe2ba4
abhithakre <68857990+abhithakre@users.noreply.github.com>
2020-09-08 15:27:23 +0530

    Update Program.cs

--- Cover/Program.cs
+++ Cover/Program.cs
@@ -6,7 +6,7 @@ namespace Cover
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World test12345!");
+            Console.WriteLine("Hello World tt!");
         }
 
         public static int Sum(int a, int b)  // <--- Important! Add the public keyword here
